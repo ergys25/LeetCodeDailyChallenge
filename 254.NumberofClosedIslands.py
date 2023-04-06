7af489bf4ff7c8148f56dc96476f71ef9425c739
ERGYS HILA <ergysmania@gmail.com>
2023-04-05 22:18:10 -0400

    Create 254.NumberofClosedIslands.py

---
+++ 254.NumberofClosedIslands.py
@@ -0,0 +1,33 @@
+"""
+Given a 2D grid consists of 0s (land) and 1s (water).  An island is a maximal 4-directionally connected group of 0s and a closed island is an island totally 
+(all left, top, right, bottom) surrounded by 1s.
+
+Return the number of closed islands.
+"""
+
+class Solution:
+    def closedIsland(self, grid: List[List[int]]) -> int:
+        rows, cols = len(grid), len(grid[0])
+        count = 0
+        
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= rows or j >= cols:
+                return False
+            if grid[i][j] == 1:
+                return True
+            grid[i][j] = 1 # mark as visited
+            left = dfs(i, j-1)
+            right = dfs(i, j+1)
+            up = dfs(i-1, j)
+            down = dfs(i+1, j)
+            return left and right and up and down
+        
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == 0 and dfs(i, j):
+                    count += 1
+        
+        return count
+
+
+
