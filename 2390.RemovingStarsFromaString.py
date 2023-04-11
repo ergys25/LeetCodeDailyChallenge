17f51b7fb241ecca250b5e3c8c6b3963d053bfb3
Ergys Hila <ergysmania@gmail.com>
2023-04-11 00:27:14 -0400

    Todays leetcode challenge

---
+++ 2390.RemovingStarsFromaString.py
@@ -0,0 +1,25 @@
+"""
+You are given a string s, which contains stars *.
+
+In one operation, you can:
+
+Choose a star in s.
+Remove the closest non-star character to its left, as well as remove the star itself.
+Return the string after all stars have been removed.
+
+Note:
+
+The input will be generated such that the operation is always possible.
+It can be shown that the resulting string will always be unique.
+"""
+
+
+class Solution:
+    def removeStars(self, s: str) -> str:
+        ans=[]
+        for i in s:
+            if i=='*':
+                ans.pop()
+            else:
+                ans+=[i]
+        return "".join(ans)
