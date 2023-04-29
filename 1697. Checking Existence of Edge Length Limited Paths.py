a2a9b47b421a6fdbd6b36a71ca2becb02e5483de
Ergys Hila <ergysmania@gmail.com>
2023-04-29 12:52:49 -0400

    1697. Checking Existence of Edge Length Limited Paths

---
+++ 1697. Checking Existence of Edge Length Limited Paths.py
@@ -0,0 +1,46 @@
+"""
+An undirected graph of n nodes is defined by edgeList, where edgeList[i] = [ui, vi, disi] denotes an edge between nodes ui and vi with distance disi. Note that there may be multiple edges between two nodes.
+
+Given an array queries, where queries[j] = [pj, qj, limitj], your task is to determine for each queries[j] whether there is a path between pj and qj such that each edge on the path has a distance strictly less than limitj .
+
+Return a boolean array answer, where answer.length == queries.length and the jth value of answer is true if there is a path for queries[j] is true, and false otherwise.
+
+"""
+
+class UnionFind:
+    def __init__(self, n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+    
+    def find(self, x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self, x, y):
+        px, py = self.find(x), self.find(y)
+        if px != py:
+            if self.rank[px] < self.rank[py]:
+                self.parent[px] = py
+            elif self.rank[px] > self.rank[py]:
+                self.parent[py] = px
+            else:
+                self.parent[py] = px
+                self.rank[px] += 1
+    
+    def connected(self, x, y):
+        return self.find(x) == self.find(y)
+
+class Solution:
+    def distanceLimitedPathsExist(self, n: int, edges: List[List[int]], queries: List[List[int]]) -> List[bool]:
+        edges = sorted(edges, key=lambda x: x[2])
+        ans = [False] * len(queries)
+        uf = UnionFind(n)
+        i = 0
+        sorted_queries = sorted(enumerate(queries), key=lambda x: x[1][2])
+        for q_idx, (u, v, limit) in sorted_queries:
+            while i < len(edges) and edges[i][2] < limit:
+                uf.union(edges[i][0], edges[i][1])
+                i += 1
+            ans[q_idx] = uf.connected(u, v)
+        return ans
