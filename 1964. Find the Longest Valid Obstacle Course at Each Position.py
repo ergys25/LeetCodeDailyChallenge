99f1edb80f4a40aee60698ada66d264027c70816
Ergys Hila <ergysmania@gmail.com>
2023-05-07 19:56:50 -0400

    1964

---
+++ 1964. Find the Longest Valid Obstacle Course at Each Position.py
@@ -0,0 +1,26 @@
+"""
+
+You want to build some obstacle courses. You are given a 0-indexed integer array obstacles of length n, where obstacles[i] describes the height of the ith obstacle.
+
+For every index i between 0 and n - 1 (inclusive), find the length of the longest obstacle course in obstacles such that:
+
+You choose any number of obstacles between 0 and i inclusive.
+You must include the ith obstacle in the course.
+You must put the chosen obstacles in the same order as they appear in obstacles.
+Every obstacle (except the first) is taller than or the same height as the obstacle immediately before it.
+Return an array ans of length n, where ans[i] is the length of the longest obstacle course for index i as described above.
+"""
+import bisect
+
+class Solution:
+    def longestObstacleCourseAtEachPosition(self, obstacles: List[int]) -> List[int]:
+        lis = []
+        result = []
+        for obstacle in obstacles:
+            idx = bisect.bisect_right(lis, obstacle)
+            if idx == len(lis):
+                lis.append(obstacle)
+            else:
+                lis[idx] = obstacle
+            result.append(idx+1)
+        return result
