6a303e4f5a3d25a56c69479e50d565d6229166d0
ERGYS HILA <ergysmania@gmail.com>
2023-06-03 07:35:34 +0300

    Create 1376.py

---
+++ 1376.py
@@ -0,0 +1,17 @@
+class Solution:
+    def numOfMinutes(self, n: int, headID: int, a: List[int], b: List[int]) -> int:
+        mp=defaultdict(list)
+        for i in range(len(a)):
+            mp[a[i]].append(i)
+        v=deque()
+        v+=[[headID,b[headID]]]
+        ans=0
+        while v:
+            x=v[0]
+            v.popleft()
+            for i in mp[x[0]]:
+                v+=[[i,x[1]+b[i]]]
+                ans=max(ans,x[1]+b[i])
+        return ans
+            
+            
