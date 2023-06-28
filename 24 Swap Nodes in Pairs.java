12e08decaff8cd199e3553999e23d3b552c1e5b4
ERGYS HILA <ergysmania@gmail.com>
2023-06-27 21:32:11 -0400

    Update 24 Swap Nodes in Pairs.java

--- 24 Swap Nodes in Pairs.java
+++ 24 Swap Nodes in Pairs.java
@@ -1,5 +1,6 @@
 
 /**
+    24
  * Given a linked list, swap every two adjacent nodes and return its head. You must solve the problem without modifying the values in the list's nodes (i.e., only nodes themselves may be changed.)
  * Definition for singly-linked list.
  * public class ListNode {
