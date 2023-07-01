ba638a95b0e6e98127b83607fd0b4bb9072cdcf1
ERGYS HILA <ergysmania@gmail.com>
2023-07-01 06:49:58 -0400

    Update 946.ValidateStack equences.py

--- 946.ValidateStack equences.py
+++ 946.ValidateStack equences.py
@@ -1,4 +1,5 @@
 """
+946
 Given two integer arrays pushed and popped each with distinct values,
 return true if this could have been the result of a sequence of push and pop operations
 on an initially empty stack, or false otherwise.
