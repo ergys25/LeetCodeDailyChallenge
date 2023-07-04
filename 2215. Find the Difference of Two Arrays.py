70649f8b9a787389c2d5b4a7cecb02282b2c68f0
ERGYS HILA <ergysmania@gmail.com>
2023-07-03 23:43:19 -0400

    Update 2215. Find the Difference of Two Arrays.py

--- 2215. Find the Difference of Two Arrays.py
+++ 2215. Find the Difference of Two Arrays.py
@@ -1,4 +1,5 @@
 """
+2215
 Given two 0-indexed integer arrays nums1 and nums2, return a list answer of size 2 where:
 
 answer[0] is a list of all distinct integers in nums1 which are not present in nums2.
