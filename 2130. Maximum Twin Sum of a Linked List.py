2baf67e06582b4059941532046db7d4f8c986edc
ERGYS HILA <ergysmania@gmail.com>
2023-07-03 23:43:41 -0400

    Update 2130. Maximum Twin Sum of a Linked List.py

--- 2130. Maximum Twin Sum of a Linked List.py
+++ 2130. Maximum Twin Sum of a Linked List.py
@@ -1,5 +1,6 @@
 
 """
+2130
 In a linked list of size n, where n is even, the ith node (0-indexed) of the linked list is known as the twin of the (n-1-i)th node, if 0 <= i <= (n / 2) - 1.
 
 For example, if n = 4, then node 0 is the twin of node 3, and node 1 is the twin of node 2. These are the only nodes with twins for n = 4.
