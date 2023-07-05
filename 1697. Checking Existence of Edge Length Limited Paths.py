ae980fad59ec5143596be16fd38ccaab67bb19ce
ERGYS HILA <ergysmania@gmail.com>
2023-07-04 23:18:07 -0400

    Update 1697. Checking Existence of Edge Length Limited Paths.py

--- 1697. Checking Existence of Edge Length Limited Paths.py
+++ 1697. Checking Existence of Edge Length Limited Paths.py
@@ -1,4 +1,5 @@
 """
+1697
 An undirected graph of n nodes is defined by edgeList, where edgeList[i] = [ui, vi, disi] denotes an edge between nodes ui and vi with distance disi. Note that there may be multiple edges between two nodes.
 
 Given an array queries, where queries[j] = [pj, qj, limitj], your task is to determine for each queries[j] whether there is a path between pj and qj such that each edge on the path has a distance strictly less than limitj .
