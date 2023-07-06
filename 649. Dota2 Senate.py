8c3ec5393b17fe2de552d5b671b9137da79c5f7e
ERGYS HILA <ergysmania@gmail.com>
2023-07-06 12:31:40 -0400

    Update 649. Dota2 Senate.py

--- 649. Dota2 Senate.py
+++ 649. Dota2 Senate.py
@@ -1,5 +1,5 @@
 """
-
+649
 In the world of Dota2, there are two parties: the Radiant and the Dire.
 
 The Dota2 senate consists of senators coming from two parties. Now the Senate wants to decide on a change in the Dota2 game. The voting for this change is a round-based procedure. In each round, each senator can exercise one of the two rights:
