8235b4dbfe5b9d2c508150d0fe6de110fd87f5c4
ERGYS HILA <ergysmania@gmail.com>
2023-07-07 13:10:11 -0400

    Update 1557. Minimum Number of Vertices to Reach All Nodes.py

--- 1557. Minimum Number of Vertices to Reach All Nodes.py
+++ 1557. Minimum Number of Vertices to Reach All Nodes.py
@@ -1,4 +1,5 @@
 """
+1557
 Given a directed acyclic graph, with n vertices numbered from 0 to n-1, and an array edges where edges[i] = [fromi, toi] represents a directed edge from node fromi to node toi.
 
 Find the smallest set of vertices from which all nodes in the graph are reachable. It's guaranteed that a unique solution exists.
