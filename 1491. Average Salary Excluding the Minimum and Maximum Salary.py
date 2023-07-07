c5413f817ac44b31495ce922c2e3f3415a975831
ERGYS HILA <ergysmania@gmail.com>
2023-07-07 13:09:44 -0400

    Update 1491. Average Salary Excluding the Minimum and Maximum Salary.py

--- 1491. Average Salary Excluding the Minimum and Maximum Salary.py
+++ 1491. Average Salary Excluding the Minimum and Maximum Salary.py
@@ -1,4 +1,5 @@
 """
+1491
 You are given an array of unique integers salary where salary[i] is the salary of the ith employee.
 
 Return the average salary of employees excluding the minimum and maximum salary. Answers within 10-5 of the actual answer will be accepted.
