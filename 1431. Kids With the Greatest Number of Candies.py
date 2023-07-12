8263b97c3f8e1113441922cc3c95aa0a3c2f12f7
ERGYS HILA <ergysmania@gmail.com>
2023-07-12 17:36:50 -0400

    Update 1431. Kids With the Greatest Number of Candies.py

--- 1431. Kids With the Greatest Number of Candies.py
+++ 1431. Kids With the Greatest Number of Candies.py
@@ -1,5 +1,5 @@
 """
-1431
+1431.py
 There are n kids with candies. You are given an integer array candies, where each candies[i] represents the number of candies the ith kid has, and an integer extraCandies, denoting the number of extra candies that you have.
 
 Return a boolean array result of length n, where result[i] is true if, after giving the ith kid all the extraCandies, they will have the greatest number of candies among all the kids, or false otherwise.
