810a623a0371f076792f9352d6012d3d63c677fd
ERGYS HILA <ergysmania@gmail.com>
2023-09-08 22:45:21 -0400

    Update 1091.py

--- 1091.py
+++ 1091.py
@@ -17,7 +17,7 @@ class Solution:
 
             directions = [
                 (i - 1, j - 1),  (i - 1, j), (i - 1, j + 1),
-                (i, j - 1), (i, j + 1),
+                (i, j - 1),  (i, j + 1),
                 (i + 1, j - 1), (i + 1, j), (i + 1, j + 1)
             ]
 
