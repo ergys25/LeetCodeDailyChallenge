7166489afeeb2941557b65d19d6f44284c015e0a
Ergys Hila <ergysmania@gmail.com>
2023-10-14 19:33:34 -0400

    edit

--- 399. Evaluate Division.py
+++ 399. Evaluate Division.py
@@ -8,7 +8,7 @@ class Solution:
         self.visited.add(current)
         if current in self.graph:
             for i in self.graph[current]:
-                if i[0] not in self.visited:
+                if i[ 0] not in self.visited:
                     a=self.answer(i[0],end,scalar*i[1])
                     if a!=-1: return a
         return -1
