3c211e37a55bc0dcf252e861b51a75e2e69f939f
ERGYS HILA <ergysmania@gmail.com>
2023-10-15 22:23:25 -0400

    Update 1232.py

--- 1232.py
+++ 1232.py
@@ -3,8 +3,8 @@
 """
 class Solution:
     def checkStraightLine(self, coordinates):
-        x0, y0 = coordinates[0]
-        x1, y1 = coordinates[1]
+        x0, y0 = coordinates[0 ]
+        x1, y1 = coordinates[ 1]
 
         for i in range(2, len(coordinates)):
             x, y = coordinates[i]
