f76b1cf698aa007347ece4bf903859ac63dc6ca7
Ergys Hila <ergysmania@gmail.com>
2023-10-21 21:51:38 -0400

    editedit

--- 399. Evaluate Division.py
+++ 399. Evaluate Division.py
@@ -6,7 +6,7 @@ class Solution:
     def answer(self, current, end, scalar):
         if current==end: return scalar
          self.visited.add(current)
-        if current in self.graph:
+        if current  in self.graph:
             for i in self.graph[current]:
                 if i[ 0] not in self.visited:
                     a=self.answer( i[0],end,scalar*i[1])
